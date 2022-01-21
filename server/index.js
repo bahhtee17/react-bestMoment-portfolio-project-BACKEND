cf611c5c307e0b3e8b706f06239a056e45a05708
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2022-01-22 02:17:11 +0500

    backend index.js was built

--- server/index.js
+++ server/index.js
@@ -2,3 +2,13 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://portfolio-project:bahhteewerty@cluster0.altr0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
