df209fcfe17315b439d75ef1089627bb5bf91ba1
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2022-01-22 14:17:21 +0500

    created routes and controller

--- server/index.js
+++ server/index.js
@@ -3,12 +3,22 @@ import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 
+import postRouter from "./routes/posts.js";
+
 const app = express();
+app.use("/posts", postRouter);
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 const CONNECTION_URL =
-  "mongodb+srv://portfolio-project:bahhteewerty@cluster0.altr0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://portfolio-project:bahhteewerty123@cluster0.altr0.mongodb.net/myFirstDatabase?retryWrites=true";
 
 const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
